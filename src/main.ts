55c728d70d653f91d8d41851ff25dc0c49f97dfb
NavodContrath <navod.contrath@gmail.com>
2025-08-13 11:38:25 +0200

    MILESTONE-3

---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import type { Actress } from "./type";
+
+function isActress(dati: unknown): dati is Actress {
+  if (
+    dati &&
+    typeof dati === "object" &&
+    "id" in dati &&
+    typeof dati.id === "number" &&
+    "name" in dati &&
+    typeof dati.name === "string" &&
+    "birth_year" in dati &&
+    typeof dati.birth_year === "number" &&
+    "death_year" in dati &&
+    typeof dati.death_year === "number" &&
+    "biography" in dati &&
+    typeof dati.biography === "string" &&
+    "image" in dati &&
+    typeof dati.image === "string" &&
+    "most_famous_movies" in dati &&
+    Array.isArray(dati.most_famous_movies) &&
+    dati.most_famous_movies.length === 3 &&
+    dati.most_famous_movies.every(movie => typeof movie === "string") &&
+    "awards" in dati &&
+    typeof dati.awards === "string" &&
+    "nationality" in dati &&
+    typeof dati.nationality === "string"
+  ) {
+    return true
+  }
+  return false
+
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`)
+    if (!response.ok) {
+      throw new Error(`HTP Error ${response.status}:${response.statusText}`)
+    }
+    const dati: unknown = await response.json()
+
+    if (!isActress(dati)) {
+      throw new Error('Formato dei dati non valido')
+    }
+    console.log(dati)
+    return dati
+  } catch (errore) {
+    if (errore instanceof Error) {
+      console.log(`Errore durante il recupero dati, ${errore.message}`)
+    }
+    return null
+
+  }
+}
+getActress(5)
